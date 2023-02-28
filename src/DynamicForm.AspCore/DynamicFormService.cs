0533577ee942e8aea2c45e099aee8785f8b57cca
Habeeb Jimoh <habeebjimoh@hotmail.com>
2023-02-28 18:33:01 +0100

    rename _formContexts

--- src/DynamicForm.AspCore/DynamicFormService.cs
+++ src/DynamicForm.AspCore/DynamicFormService.cs
@@ -1,18 +1,18 @@
 namespace DynamicForm.AspCore;
 public class DynamicFormService : IDynamicFormService
 {
-    private readonly IEnumerable<FormCollection> _formContexts;
+    private readonly IEnumerable<FormCollection> _formCollection;
 
     public DynamicFormService(IEnumerable<FormCollection> formContexts)
     {
-        _formContexts = formContexts;
+        _formCollection = formContexts;
     }
 
     public IEnumerable<object> GetForms()
     {
-        return _formContexts.Select(x => new { Id = x.CollectionName, FormContext = x.GetType().Name });
+        return _formCollection.Select(x => new { Id = x.CollectionName, FormContext = x.GetType().Name });
     }
 
     public Dictionary<string, object>? GetForm(string id) =>
-        _formContexts.FirstOrDefault(x => string.Compare(x.CollectionName, id, ignoreCase: true) == 0)?.Build();
+        _formCollection.FirstOrDefault(x => string.Compare(x.CollectionName, id, ignoreCase: true) == 0)?.Build();
 }
