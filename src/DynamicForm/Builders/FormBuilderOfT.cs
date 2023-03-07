be955119663172d5b8776e48dad8471d64911e95
Habeeb Jimoh <habeebjimoh@hotmail.com>
2023-03-07 09:45:14 +0100

    fix warning

--- src/DynamicForm/Builders/FormBuilderOfT.cs
+++ src/DynamicForm/Builders/FormBuilderOfT.cs
@@ -5,7 +5,7 @@ namespace DynamicForm
 {
     public class FormBuilder<TModel> : FormBuilder, IFormBuilder<TModel> where TModel : notnull
     {
-        public IInputBuilder<TModel, TProperty> Property<TProperty>(Expression<Func<TModel, TProperty>> propertyExpression, InputType inputType = InputType.Text)
+        public IInputBuilder<TModel, TProperty> Property<TProperty>(Expression<Func<TModel, TProperty>> propertyExpression, InputType inputType = InputType.Text) where TProperty : notnull
         {
             var propertyName = ((MemberExpression)propertyExpression.Body)?.Member.Name;
             ArgumentNullException.ThrowIfNull(propertyName, nameof(propertyName));
@@ -13,37 +13,37 @@ namespace DynamicForm
             return Property<TProperty>(propertyName, inputType);
         }
 
-        public IInputBuilder<TModel, TProperty> Property<TProperty>(string propertyName, InputType inputType = InputType.Text)
+        public IInputBuilder<TModel, TProperty> Property<TProperty>(string propertyName, InputType inputType = InputType.Text) where TProperty : notnull
         {
             return base.Property<TModel, TProperty>(propertyName, inputType.ToString());
         }
 
-        public IInputBuilder<TModel, TProperty> EmailField<TProperty>(Expression<Func<TModel, TProperty>> propertyExpression)
+        public IInputBuilder<TModel, TProperty> EmailField<TProperty>(Expression<Func<TModel, TProperty>> propertyExpression) where TProperty : notnull
         {
             return Property(propertyExpression, InputType.Email);
         }
 
-        public IInputBuilder<TModel, TProperty> PasswordField<TProperty>(Expression<Func<TModel, TProperty>> propertyExpression)
+        public IInputBuilder<TModel, TProperty> PasswordField<TProperty>(Expression<Func<TModel, TProperty>> propertyExpression) where TProperty : notnull
         {
             return Property(propertyExpression, InputType.Password);
         }
 
-        public IInputBuilder<TModel, TProperty> NumberField<TProperty>(Expression<Func<TModel, TProperty>> propertyExpression)
+        public IInputBuilder<TModel, TProperty> NumberField<TProperty>(Expression<Func<TModel, TProperty>> propertyExpression) where TProperty : notnull
         {
             return Property(propertyExpression, InputType.Number);
         }
 
-        public IInputBuilder<TModel, TProperty> TextAreaField<TProperty>(Expression<Func<TModel, TProperty>> propertyExpression)
+        public IInputBuilder<TModel, TProperty> TextAreaField<TProperty>(Expression<Func<TModel, TProperty>> propertyExpression) where TProperty : notnull
         {
             return Property(propertyExpression, InputType.TextArea);
         }
 
-        public IInputBuilder<TModel, TProperty> TextField<TProperty>(Expression<Func<TModel, TProperty>> propertyExpression)
+        public IInputBuilder<TModel, TProperty> TextField<TProperty>(Expression<Func<TModel, TProperty>> propertyExpression) where TProperty : notnull
         {
             return Property(propertyExpression, InputType.Text);
         }
 
-        public IInputBuilder<TModel, TProperty> ConfirmField<TProperty>(Expression<Func<TModel, TProperty>> propertyExpression, InputType type)
+        public IInputBuilder<TModel, TProperty> ConfirmField<TProperty>(Expression<Func<TModel, TProperty>> propertyExpression, InputType type) where TProperty : notnull
         {
             var property = ((MemberExpression)propertyExpression.Body)?.Member.Name;
             ArgumentNullException.ThrowIfNull(property, nameof(property));
@@ -53,12 +53,12 @@ namespace DynamicForm
             return Property<TModel, TProperty>($"Confirm{property}", type.ToString(), additionalFields);
         }
 
-        public IOptionBuilder<TModel, TProperty> CheckBox<TProperty>(Expression<Func<TModel, TProperty>> propertyExpression)
+        public IOptionBuilder<TModel, TProperty> CheckBox<TProperty>(Expression<Func<TModel, TProperty>> propertyExpression) where TProperty : notnull
         {
             return (IOptionBuilder<TModel, TProperty>)Property(propertyExpression, InputType.CheckBox);
         }
 
-        public IOptionBuilder<TModel, TProperty> Select<TProperty>(Expression<Func<TModel, TProperty>> propertyExpression)
+        public IOptionBuilder<TModel, TProperty> Select<TProperty>(Expression<Func<TModel, TProperty>> propertyExpression) where TProperty : notnull
         {
             return (IOptionBuilder<TModel, TProperty>)Property(propertyExpression, InputType.Select);
         }
--- src/DynamicForm/Builders/Interfaces/IFormBuilderOfT.cs
+++ src/DynamicForm/Builders/Interfaces/IFormBuilderOfT.cs
@@ -4,17 +4,17 @@ namespace DynamicForm.Interfaces
 {
     public interface IFormBuilder<TModel> where TModel : notnull
     {
-        IInputBuilder<TModel, TProperty> TextField<TProperty>(Expression<Func<TModel, TProperty>> propertyExpression);
-        IInputBuilder<TModel, TProperty> EmailField<TProperty>(Expression<Func<TModel, TProperty>> propertyExpression);
-        IInputBuilder<TModel, TProperty> NumberField<TProperty>(Expression<Func<TModel, TProperty>> propertyExpression);
-        IInputBuilder<TModel, TProperty> PasswordField<TProperty>(Expression<Func<TModel, TProperty>> propertyExpression);
-        IInputBuilder<TModel, TProperty> TextAreaField<TProperty>(Expression<Func<TModel, TProperty>> propertyExpression);
-        IInputBuilder<TModel, TProperty> Property<TProperty>(string propertyName, InputType inputType = InputType.Text);
-        IInputBuilder<TModel, TProperty> ConfirmField<TProperty>(Expression<Func<TModel, TProperty>> propertyExpression, InputType type);
-        IInputBuilder<TModel, TProperty> Property<TProperty>(Expression<Func<TModel, TProperty>> propertyExpression, InputType inputType = InputType.Text);
+        IInputBuilder<TModel, TProperty> TextField<TProperty>(Expression<Func<TModel, TProperty>> propertyExpression) where TProperty : notnull;
+        IInputBuilder<TModel, TProperty> EmailField<TProperty>(Expression<Func<TModel, TProperty>> propertyExpression) where TProperty : notnull;
+        IInputBuilder<TModel, TProperty> NumberField<TProperty>(Expression<Func<TModel, TProperty>> propertyExpression) where TProperty : notnull;
+        IInputBuilder<TModel, TProperty> PasswordField<TProperty>(Expression<Func<TModel, TProperty>> propertyExpression) where TProperty : notnull;
+        IInputBuilder<TModel, TProperty> TextAreaField<TProperty>(Expression<Func<TModel, TProperty>> propertyExpression) where TProperty : notnull;
+        IInputBuilder<TModel, TProperty> Property<TProperty>(string propertyName, InputType inputType = InputType.Text) where TProperty : notnull;
+        IInputBuilder<TModel, TProperty> ConfirmField<TProperty>(Expression<Func<TModel, TProperty>> propertyExpression, InputType type) where TProperty : notnull;
+        IInputBuilder<TModel, TProperty> Property<TProperty>(Expression<Func<TModel, TProperty>> propertyExpression, InputType inputType = InputType.Text) where TProperty : notnull;
 
-        IOptionBuilder<TModel, TProperty> CheckBox<TProperty>(Expression<Func<TModel, TProperty>> propertyExpression);
-        IOptionBuilder<TModel, TProperty> Select<TProperty>(Expression<Func<TModel, TProperty>> propertyExpression);
+        IOptionBuilder<TModel, TProperty> CheckBox<TProperty>(Expression<Func<TModel, TProperty>> propertyExpression) where TProperty : notnull;
+        IOptionBuilder<TModel, TProperty> Select<TProperty>(Expression<Func<TModel, TProperty>> propertyExpression) where TProperty : notnull;
         IFormInputBuilder<TModel, TProperty> FormInput<TProperty>(Expression<Func<TModel, IEnumerable<TProperty>>> propertyExpression) where TProperty : class;
     }
 }
